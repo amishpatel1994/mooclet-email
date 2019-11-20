a7f932a9b3b46895ed42aa3d0bf6a88512610269
Ronak Patel <ronak1997@gmail.com>
2019-11-20 16:18:21 -0500

    Integrated MailGun with MOOClet

--- src/mailer/views.py
+++ src/mailer/views.py
@@ -3,20 +3,27 @@ from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.core.mail import send_mail
+import requests
+import os
 # Create your views here.
 
 @api_view(['POST'])
 def send_email(request):
 	body = request.data
-	sender_email = "Amish Patel <av2patel94@gmail.com>"
-	#TODO: Make the API call to MOOClet here to get email content for the email recepients
+	sender_email = "Ronak Patel <ronak1997@gmail.com>"
+	BASE_URL = "https://celery.mooclet.com/engine/api/v1/mooclet/"
+	mooclet_id = body["id"]
+	URL = BASE_URL + mooclet_id + "/run?user_id="
 	email_recepients = body["emails"]
-	dummy_subject = "It works!"
-	dummy_content = "This will get sent through Mailgun"
-	for email in email_recepients:
-		send_mail(dummy_subject, dummy_content, sender_email, [email])
 
-	return Response(status=200, data="Emails sent.")
+	for email in email_recepients:
+		request_URL = URL+email
+		mooclet_response = requests.get(request_URL, headers={'Authorization': os.getenv("MOOCLET_API_KEY")})
+		email_subject = mooclet_response.json()["name"]
+		email_body = mooclet_response.json()["text"]
+		send_mail(email_subject, email_body, sender_email, [email])
+
+	return Response(status=200, data="Emails successfully sent!")
 
 @api_view(['POST'])
 def new_email_metric(request):
